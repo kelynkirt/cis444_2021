1acbe7765b553b66e9640b3fa0496280da5bcfc4
Kelyn Kirt <44416216+kelynkirt@users.noreply.github.com>
2021-12-15 11:13:26 -0800

    Update app.py

--- flask_jwt_rest_server/app.py
+++ flask_jwt_rest_server/app.py
@@ -36,7 +36,7 @@ def init_new_env():
 def index():
     return redirect('/static/index.html')
 
-@app.route("/login", methods=['GET','POST'])
+@app.route("/login", methods=['GET'])
 def login():
     return redirect('/static/home.html')
 
