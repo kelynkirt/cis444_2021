a11506b4a88d44d23b289493ca34b0d06902b7ca
Kelyn Kirt <44416216+kelynkirt@users.noreply.github.com>
2021-12-15 10:48:22 -0800

    Update app.py

--- flask_jwt_rest_server/app.py
+++ flask_jwt_rest_server/app.py
@@ -38,7 +38,7 @@ def index():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    return redirect('index.html')
+    return render_template('home.html')
 
 
 @app.route("/secure_api/<proc_name>",methods=['GET', 'POST'])
